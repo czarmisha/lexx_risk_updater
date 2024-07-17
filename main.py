fa0caed6ef1dc086efb2fe85e7f9c9a5cffa9a01
m.dergachyov <m.dergachyov@aigroup.uz>
2024-07-17 23:13:52 +0500

    takion api test get accounts

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ def main():
     docs_api = DocsAPIClient()
     api_client = TakionAPIClient()
 
+    accounts = api_client.get_accounts()  # TODO: Try except
+    if not accounts:
+        print("No accounts found")
+        # TODO: Send to telegram
+        return
+    
     sheet_data = docs_api.get_google_sheet_data()
 
-    # for index, row in df.iterrows():
-    #     data = row.to_dict()
-    #     response = api_client.send_request(endpoint="your_endpoint", data=data)
-    #     print(f"Response for row {index}: {response}")
 
 if __name__ == "__main__":
     main()
--- sdk/takion.py
+++ sdk/takion.py
@@ -10,18 +10,66 @@ class TakionAPIClient:
 
     def get_headers(self):
         return {
-            'Authorization': f"Bearer {self.token}",
+            'Authorization': f"Token {self.token}",
             'Content-Type': 'application/json'
         }
 
-    def update_account(self, data: UpdateAccount):
-        url = f"{self.base_url}/your_endpoint"
+    def update_account(self, user_id: str, data: UpdateAccount):
+        url = f"{self.base_url}{user_id}/"
         headers = self.get_headers()
-        response = requests.post(url, json=data, headers=headers)
+        response = requests.patch(url, json=data, headers=headers)
+        response.raise_for_status()
         return response.json()
     
-    def get_account(self, account_id: int):
-        url = f"{self.base_url}/your_endpoint"
+    def get_accounts(self):
+        url = f"{self.base_url}"
         headers = self.get_headers()
         response = requests.get(url, headers=headers)
+        response.raise_for_status()
         return response.json()
+
+
+
+# import requests
+# import config as cfg
+
+
+# def get_risk_list() -> list:
+#     response: requests.Response = requests.request("GET", cfg.url, headers=cfg.header)
+#     response_list: list = response.json()
+#     return response_list
+
+
+# def send_new_trade_level(user_id: int, new_constraints: dict):
+#     response: requests.Response = requests.request("PATCH", f'{cfg.url}{user_id}/', headers=cfg.header,
+#                                                    json=new_constraints)
+
+#     print(response.text)
+
+
+# def get_dict_by_account(risk_list: list, account: str) -> dict:
+#     for trade_level in risk_list:
+#         if trade_level['user']['account'] == account:
+#             return trade_level
+
+#     return {}
+
+
+# def sample():
+#     risk_list: list = get_risk_list()
+#     my_account: str = '31206519'
+
+#     my_dict: dict = get_dict_by_account(risk_list, my_account)
+
+#     if my_dict == {}:
+#         print('Account exists error', my_account)
+#         return 0
+
+#     user_id: int = my_dict['user']["id"]
+#     new_constraints: dict = {"max_loss_close": 4300}
+
+#     send_new_trade_level(user_id, new_constraints)
+
+
+# if __name__ == '__main__':
+#     sample()
