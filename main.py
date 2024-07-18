41be33bc464a97ec9cd51687c3c7d3b712ac1e55
m.dergachyov <m.dergachyov@aigroup.uz>
2024-07-18 21:28:49 +0500

    add scheduler

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
-from sdk import *
+import time
+from datetime import datetime as dt
+
+import schedule
+
+from models.api import UpdateAccount
+from sdk import DocsAPIClient, TakionAPIClient
 
 
-def main():
+def main(number: int = 1):
+    print(f"Running the main function for the {number} time at {dt.now().strftime('%Y-%m-%d %H:%M:%S')}")
     docs_api = DocsAPIClient()
     api_client = TakionAPIClient()
 
     accounts = api_client.get_accounts()  # TODO: Try except
     if not accounts:
-        print("No accounts found")
+        print("No accounts found in the API")
         # TODO: Send to telegram
         return
-    
+
     sheet_data = docs_api.get_google_sheet_data()
+    if not sheet_data:
+        print("No data found in the sheet")
+        # TODO: Send to telegram
+        return
+
+    for row in sheet_data:
+        user_id = row.get("Account")
+        if not user_id:
+            print("No Account found in the row")
+            # TODO: Send to telegram
+            continue
+
+        account = api_client.get_account(accounts, user_id)
+        if not account:
+            print(f"Account with user_id {user_id} not found in the data from API")
+            # TODO: Send to telegram
+            continue
+
+        new_constraints = {
+            "max_loss_close": row.get(f"max_loss_close_{number}"),
+            "max_loss_open": row.get(f"max_loss_open_{number}"),
+            "max_profit_close": row.get(f"max_profit_close_{number}"),
+            "max_profit_open": row.get(f"max_profit_open_{number}"),
+        }
+
+        for constraint, value in new_constraints.items():
+            if value is None:
+                print(f"Invalid value for {constraint} in the row")
+                # TODO: Send to telegram
+                continue
+            if value == account.get(constraint):  # TODO: check types
+                print(f"Value for {constraint} is already set to {value}")
+                del new_constraints[constraint]
+
+        data = UpdateAccount(**new_constraints)
+        api_client.update_account(account.get("user_id"), data)
+        print(f"Account with user_id {user_id} updated successfully")
 
 
 if __name__ == "__main__":
-    main()
+    schedule.every().day.at("12:22", "America/New_York").do(main, number=1)
+    schedule.every().day.at("12:23", "America/New_York").do(main, number=2)
+    schedule.every().day.at("12:24", "America/New_York").do(main, number=3)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    # main()
--- sdk/takion.py
+++ sdk/takion.py
@@ -10,17 +10,17 @@ class TakionAPIClient:
 
     def get_headers(self):
         return {
-            'Authorization': f"Token {self.token}",
-            'Content-Type': 'application/json'
+            "Authorization": f"Token {self.token}",
+            "Content-Type": "application/json",
         }
 
-    def update_account(self, user_id: str, data: UpdateAccount):
+    def update_account(self, user_id: int, data: UpdateAccount):
         url = f"{self.base_url}{user_id}/"
         headers = self.get_headers()
         response = requests.patch(url, json=data, headers=headers)
         response.raise_for_status()
         return response.json()
-    
+
     def get_accounts(self):
         url = f"{self.base_url}"
         headers = self.get_headers()
@@ -28,48 +28,88 @@ class TakionAPIClient:
         response.raise_for_status()
         return response.json()
 
+    @classmethod
+    def get_account(cls, accounts: list, user_id: str):
+        for account in accounts:
+            if account.get("user", {}).get("account") == user_id:
+                return account
+        return None
 
-
-# import requests
-# import config as cfg
-
-
-# def get_risk_list() -> list:
-#     response: requests.Response = requests.request("GET", cfg.url, headers=cfg.header)
-#     response_list: list = response.json()
-#     return response_list
-
-
-# def send_new_trade_level(user_id: int, new_constraints: dict):
-#     response: requests.Response = requests.request("PATCH", f'{cfg.url}{user_id}/', headers=cfg.header,
-#                                                    json=new_constraints)
-
-#     print(response.text)
-
-
-# def get_dict_by_account(risk_list: list, account: str) -> dict:
-#     for trade_level in risk_list:
-#         if trade_level['user']['account'] == account:
-#             return trade_level
-
-#     return {}
-
-
-# def sample():
-#     risk_list: list = get_risk_list()
-#     my_account: str = '31206519'
-
-#     my_dict: dict = get_dict_by_account(risk_list, my_account)
-
-#     if my_dict == {}:
-#         print('Account exists error', my_account)
-#         return 0
 
 #     user_id: int = my_dict['user']["id"]
 #     new_constraints: dict = {"max_loss_close": 4300}
 
-#     send_new_trade_level(user_id, new_constraints)
 
-
-# if __name__ == '__main__':
-#     sample()
+# example of account data
+# {
+#     "id":2108,
+#     "user":{
+#         "id":1175,
+#         "username":"IAMB",
+#         "first_name":"Yuriy",
+#         "last_name":"Bogatov",
+#         "account":"34506000",
+#         "trainee":false,
+#         "status":"M",
+#         "trader_status":2,
+#         "groups":[
+#         {
+#             "id":7,
+#             "name":"Lexx International"
+#         }
+#         ]
+#     },
+#     "max_loss":100,
+#     "max_loss_fixed":100,
+#     "lock_gain":"None",
+#     "max_loss_close":0,
+#     "max_loss_close_fixed":0,
+#     "lock_gain_auto_close":"None",
+#     "pos_loss":0,
+#     "pos_loss_fixed":0,
+#     "max_loss_symb_close":0,
+#     "max_loss_symb_close_fixed":0,
+#     "buying_power":0,
+#     "fixed_bp":0,
+#     "intraday_equity_multiplier":1.0,
+#     "day_pos_shares":100000,
+#     "day_pos_shares_fixed":100000,
+#     "max_pos_open":0,
+#     "day_pos_investment":0,
+#     "order_size":100000,
+#     "order_size_fixed":100000,
+#     "min_share_price":0,
+#     "max_share_price":1000000,
+#     "overnight_bp":0,
+#     "max_shares_traded":0,
+#     "max_shares_traded_fixed":0,
+#     "max_order_investment":5000000,
+#     "min_short_price":0.1,
+#     "morning_bp":0,
+#     "morning_long_inv":0,
+#     "morning_short_inv":0,
+#     "morning_pos_inv":0,
+#     "morning_pos_shares":0,
+#     "morning_max_loss":0,
+#     "morning_max_loss_symbol":0,
+#     "nite_long_inv":0,
+#     "nite_short_inv":0,
+#     "nite_max_loss":0,
+#     "nite_max_loss_symbol":0,
+#     "nite_pos_inv":0,
+#     "nite_pos_shares":0,
+#     "c_aggr_price":18,
+#     "options_bp":1000,
+#     "option_order_value":100,
+#     "option_order_size":5,
+#     "option_contracts_open":5,
+#     "naked_option_sell":0,
+#     "option_limit_price_tolerance":"None",
+#     "reject_below_adv":0,
+#     "total_house_margin":0,
+#     "position_house_margin":0,
+#     "max_shares_total":0,
+#     "max_shares_total_fixed":0,
+#     "options":false,
+#     "api":"None"
+# }
